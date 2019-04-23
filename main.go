92e06e03586a0711aa9c022ce96bd40ecc399f20
Casey Erdmann <14339392+3ndG4me@users.noreply.github.com>
2019-04-23 14:14:49 -0400

    Create main.go

---
+++ main.go
@@ -0,0 +1,115 @@
+package main
+
+import(
+    "net"
+    "fmt"
+    "time"
+    "os"
+    "strings"
+    "strconv"
+)
+
+
+
+func parseIPRange(cidr string) ([]string, error) {
+    
+    ip, ipnet, err := net.ParseCIDR(cidr)
+    
+    if err != nil {
+        return nil, err
+    }
+
+    var ips []string 
+    for ip := ip.Mask(ipnet.Mask); ipnet.Contains(ip); inc(ip) {
+        ips = append(ips, ip.String())
+    } 
+    // remove network address and broadcast address
+    return ips[1 : len(ips)-1], nil
+}
+
+// Function to increment the IP for the for llop in the CIDR range parser
+
+func inc(ip net.IP){
+    for j := len(ip) - 1; j >= 0; j-- {
+        ip[j]++
+        if ip[j] > 0{
+            break
+        }
+    }
+}
+
+
+func createPortRange(portRange []string) ([]int, error){
+    
+    if len(portRange) < 2{
+        port, err := strconv.Atoi(portRange[0])
+        
+        if err != nil{
+            fmt.Println(err)
+        }
+        return []int{port}, nil
+    }
+
+    range_start, err := strconv.Atoi(portRange[0])
+    if err != nil{
+        fmt.Println(err)
+    }
+    range_end, err := strconv.Atoi(portRange[1])
+    if err != nil{
+        fmt.Println(err)
+    }
+    var portList []int
+    for port := range_start; port <= range_end; port++ {
+        
+        portList = append(portList, port)
+        
+    }
+    
+    return portList, nil
+
+
+}
+
+func main(){
+    // Get UP/CIDR range from args
+    if len(os.Args) < 3{
+        fmt.Println("Usage portscanner <IP or IP range> <port or port range>")
+        fmt.Println("Example: portscanner 192.168.2.3 1-1024")
+        os.Exit(0)
+    }
+    ip := string(os.Args[1])    
+    port := string(os.Args[2])    
+     
+    portRange := strings.Split(port, "-")
+
+    portList, err := createPortRange(portRange)
+    if err != nil{
+        fmt.Println(err)
+    }
+
+    ips, err := parseIPRange(ip)
+    if err != nil {
+        fmt.Println(err)
+    }
+    
+    if len(ips) == 0 {
+        ips = append(ips, ip)
+    }
+      
+    for _, target := range ips{
+        for _, port := range portList{
+            conn, err := net.DialTimeout("tcp", target + ":" + strconv.Itoa(port), time.Duration(1) * time.Second)
+            if err != nil{
+                fmt.Println(err)
+            }
+            if conn == nil{
+                fmt.Println("Could not connect to " + target + " on port " + strconv.Itoa(port))
+            }else{
+                fmt.Println("Connected to" + target + " on port " + strconv.Itoa(port))
+                fmt.Println(conn)
+            }
+        }
+    }
+
+}
+
